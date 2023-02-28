ce30d74b2a7fa0df076863788485b6e1598263f6
Sasha <samsiki50@gmail.com>
2023-02-28 14:39:31 +0300

    Fix Lab

--- Lab10/src/Task01/Philosopher.java
+++ Lab10/src/Task01/Philosopher.java
@@ -1,35 +1,29 @@
 package Task01;
 
-import java.util.Random;
-
 class Philosopher extends Thread {
     private Object leftFork;
     private Object rightFork;
-    private Random random;
     private boolean stop;
-
     public Philosopher(Object leftFork, Object rightFork) {
         this.leftFork = leftFork;
         this.rightFork = rightFork;
-        this.random = new Random();//используется для рандома времени
         this.stop = false;
     }
-
     public void stopPhilosopher() {
         stop = true;
     }
-
     public void run() {
+        int count = 0;
         try {
             while (!stop) {//проверка на то, что программа не остановлена
                 System.out.println(Thread.currentThread().getName() + " филосов мыслит");
-                Thread.sleep(random.nextInt(1000));
+                Thread.sleep(1000);
                 System.out.println(Thread.currentThread().getName() + " филосов взял левую вилку");
                 synchronized (leftFork) {
                     System.out.println(Thread.currentThread().getName() +  " филосов взял правую вилку");
                     synchronized (rightFork) {
-                        System.out.println(Thread.currentThread().getName() + " кушает");
-                        Thread.sleep(random.nextInt(1000));
+                        System.out.println(Thread.currentThread().getName() + " кушает" + ++count);
+                        Thread.sleep(1000);
                     }
                 }
             }
